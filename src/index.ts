65bea35946487bb1cad874abab6c1a61d6853805
Omoyajowo Temidayo <temideewan@gmail.com>
2023-06-19 11:29:26 +0100

    set up knex - postgresql interface connection

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import "dotenv/config"
-import { onDatabaseConnect } from "./config/knex";
-onDatabaseConnect()
-  .then(() => console.log("Database is connected"))
-  .catch((e) => {
-    console.log("error occurred");
-    console.log(e);
-  });
+import knex, { onDatabaseConnect } from "./config/knex";
+
+const main = async () => {
+  try {
+    await onDatabaseConnect()
+    console.log("database connected")
+    await knex("urls").where("id", "62c60a").delete();
+    const urls = await knex("urls")
+    console.log(urls);
+    
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
