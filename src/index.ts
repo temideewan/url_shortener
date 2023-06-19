7161bbdb347fedd49de7d905da39f7b3637732cc
Omoyajowo Temidayo <temideewan@gmail.com>
2023-06-19 15:22:29 +0100

    create types for database models for better autocompletion and type safety

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,6 @@ const main = async () => {
   try {
     await onDatabaseConnect()
     console.log("database connected")
-    await knex("urls").where("id", "62c60a").delete();
-    const urls = await knex("urls")
-    console.log(urls);
-    
   } catch (error) {
     console.log(error);
   }
